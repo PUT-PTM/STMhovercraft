208ab82dc6e84d0f413de8a1d845539b4e3bcbf4
wapet1995 <maciej.r.marciniak@student.put.poznan.pl>
2016-05-10 11:48:53 +0200

    plik z konfuguracjami

--- main.c
+++ main.c
@@ -32,6 +32,7 @@
 #include "stm32f4xx_syscfg.h"
 #include "stm32f4xx_exti.h"
 #include "stm32_ub_hcsr04.h"
+#include "konfiguracje.h"
 
 static __IO uint32_t TimingDelay; // zmienna pomocna do zegara SysTick
 volatile uint16_t dane_serwo = 1200; // wartosc srodkowa serwa
@@ -59,91 +60,6 @@ void TimingDelay_Decrement(void) {
 }
 void SysTick_Handler(void) {
 	TimingDelay_Decrement();
-}
-
-/*******************************************************************************************************
- Funckja inicjalizujaca linie sterujace
- *******************************************************************************************************/
-void Zegar() {
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOE, ENABLE);
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART3, ENABLE);
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
-}
-
-/*******************************************************************************************************
- Funkcja inicjalizujaca porty GPIO
- ********************************************************************************************************/
-void GPIO() {
-	/* piny sterujace kierunkami silnikow */
-	GPIO_InitTypeDef GPIO_InitStructure;
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_10 | GPIO_Pin_9
-			| GPIO_Pin_7;
-	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
-	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
-	GPIO_Init(GPIOE, &GPIO_InitStructure);
-
-	/* piny PWM dla serwa i obu silnikow */
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7 | GPIO_Pin_8;
-	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
-	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
-	GPIO_Init(GPIOB, &GPIO_InitStructure);
-}
-
-/****************************************************************************************************
- Funkcja konfigurujaca USART
- ****************************************************************************************************/
-void Config_USART() {
-	USART_InitTypeDef USART_InitStructure;
-	USART_InitStructure.USART_BaudRate = 9600;
-	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
-	USART_InitStructure.USART_StopBits = USART_StopBits_1;
-	USART_InitStructure.USART_Parity = USART_Parity_No;
-	USART_InitStructure.USART_HardwareFlowControl =
-			USART_HardwareFlowControl_None;
-	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
-	USART_Init(USART3, &USART_InitStructure);
-}
-
-/******************************************************************************************************
- Funkcje konfigurujace piny TX i RX wykorzystywane przy transmisji danych
- *******************************************************************************************************/
-void Config_Tx() {
-	// konfiguracja linii Tx
-	GPIO_PinAFConfig(GPIOC, GPIO_PinSource10, GPIO_AF_USART3);
-	GPIO_InitTypeDef GPIO_InitStructure;
-	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_Init(GPIOC, &GPIO_InitStructure);
-}
-
-void Config_Rx() {
-	// konfiguracja linii Rx
-	GPIO_PinAFConfig(GPIOC, GPIO_PinSource11, GPIO_AF_USART3);
-	GPIO_InitTypeDef GPIO_InitStructure;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_11;
-	GPIO_Init(GPIOC, &GPIO_InitStructure);
-}
-
-/*********************************************************************************************************
- Funkcja konfigurujaca przerwania dla USART
- **********************************************************************************************************/
-void Config_NVIC() {
-	NVIC_InitTypeDef NVIC_InitStructure;
-	USART_ITConfig(USART3, USART_IT_RXNE, ENABLE);
-	NVIC_InitStructure.NVIC_IRQChannel = USART3_IRQn;
-	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
-	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
-	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-	NVIC_Init(&NVIC_InitStructure);
 }
 
 /************************************************************************************************
@@ -199,61 +115,6 @@ void USART3_IRQHandler(void) {
 		while (USART_GetFlagStatus(USART3, USART_FLAG_TC) == RESET) {
 		}
 	}
-}
-
-/***************************************************************************************************
- Funckja inicjalizujaca zegar TIM4 dla PWM
- ***************************************************************************************************/
-void Timer4() {
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE);
-	TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
-	TIM_TimeBaseStructure.TIM_Period = 60000;
-	TIM_TimeBaseStructure.TIM_Prescaler = 83;
-	TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;
-	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
-	TIM_TimeBaseInit(TIM4, &TIM_TimeBaseStructure);
-	TIM_Cmd(TIM4, ENABLE);
-}
-
-/***************************************************************************************************
- Funkcja inicjalizujaca PWM
- ***************************************************************************************************/
-void PWM() {
-	TIM_OCInitTypeDef TIM_OCInitStructure;
-	GPIO_InitTypeDef GPIO_InitStructure;
-
-	TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
-	TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
-	TIM_OCInitStructure.TIM_Pulse = 0;
-	TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
-
-	/* KONFIGURACJA 1 KANALU */
-	TIM_OC1Init(TIM4, &TIM_OCInitStructure);
-	TIM_OC1PreloadConfig(TIM4, TIM_OCPreload_Enable);
-	GPIO_PinAFConfig(GPIOB, GPIO_PinSource6, GPIO_AF_TIM4);
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
-	GPIO_Init(GPIOB, &GPIO_InitStructure);
-
-	/* KONFIGURACJA 2 KANALU */
-	TIM_OC2Init(TIM4, &TIM_OCInitStructure);
-	TIM_OC2PreloadConfig(TIM4, TIM_OCPreload_Enable);
-	GPIO_PinAFConfig(GPIOB, GPIO_PinSource7, GPIO_AF_TIM4);
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_7;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
-	GPIO_Init(GPIOB, &GPIO_InitStructure);
-
-	/* KONFIGURACJA 3 KANALU */
-	TIM_OC3Init(TIM4, &TIM_OCInitStructure);
-	TIM_OC3PreloadConfig(TIM4, TIM_OCPreload_Enable);
-	GPIO_PinAFConfig(GPIOB, GPIO_PinSource8, GPIO_AF_TIM4);
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
-	GPIO_Init(GPIOB, &GPIO_InitStructure);
-
-	GPIO_SetBits(GPIOB, GPIO_Pin_6);
-	GPIO_SetBits(GPIOB, GPIO_Pin_7);
-	GPIO_SetBits(GPIOB, GPIO_Pin_8);
 }
 
 /*******************************************************************************************
@@ -324,6 +185,6 @@ int main(void) {
 		}
 		if(licznik < 10000)
 		licznik += 150; // zwieksza licznik o ilosc milisekund opoznienia
-		odl = (uint8_t)((UB_HCSR04_Distance_cm()+UB_HCSR04_Distance_cm())/2); //odczytywanie odleglosci z czujnika odleglosci HC-Sr04
+		odl = (uint8_t)(UB_HCSR04_Distance_cm()); //odczytywanie odleglosci z czujnika odleglosci HC-Sr04
 	}
 }
